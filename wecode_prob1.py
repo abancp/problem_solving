c74ca80a5b8c1de621f777fa89c7afe336ce91e9
Aban Muhammed C P <abancpchengani@gmail.com>
2024-01-13 22:19:31 +0530

    algo upgraped

--- wecode_prob1.py
+++ wecode_prob1.py
@@ -1,119 +1,69 @@
-"""
-problem : Given a list of numbers arr of size n.
-and another table called lookup with number to list of numbers key-value pair. (this value list
-can be of any variable size.)
-you have to return the {keys} that has the maximum occurences of the given numbers in arr.
-                           ^ so return a array of keys
+import datetime
+import json
 
-return a list keys 
-"""
-
-
-def solution(n, arr, lookup):
-    maxi = None
-    res_map = {}
-    hash_map = {}
-    for i in arr:
-        hash_map[i] = i
-    for i in lookup:
-        res_map[i] = 0
-        for j in lookup[i]:
-            if j in hash_map:
-                res_map[i] += 1
-        if maxi == None:
-            maxi = i
-        else:
-            if res_map[maxi] > res_map[i]:
-                maxi = maxi
-            else:
-                maxi = i
-    return [k for k, v in res_map.items() if v == res_map[maxi]]
-
+s = datetime.datetime.now()
 
 """
-solution is O(sum of length of all row in table) -> O(n) Time complex
-     and O(length of array) -> O(n) Space Complex
+ _________________________________________________
+|_____testcase____|___size___|__ time__|__result__|
+| small_testcase1 |   7.4 mb |  0.21 s | ['49']   |
+| small_testcase2 |   6.4 mb |  0.17 s | ['37']   |
+| small_testcase3 |  26.9 mb |  0.77 s | ['75']   |
+| small_testcase4 |  21.4 mb |  0.62 s | ['145']  |
+|_small_testcase5_|_ 33.0_mb_|__1.04 s_|_['184']__|
+|_________________________________________________|
+| large_testcase1 | 206.0 mb |  5.07 s | ['1033'] |
+| large_testcase2 | 389.6 mb |  ---- s | ['---']  |
+| large_testcase3 |  81.1 mb |  1.92 s | ['715']  |
+| large_testcase4 | 167.4 mb |  0.21 s | ['911']  |
+|_large_testcase5_|_111.0_mb_|__2.95 s_|_['250']__|
+|_________________|__________|_________|__________|
+
 """
 
 
-def test_solution():
-    test_cases = [
-        {
-            "arr": [1, 12, 0, 3, 92, 4, 28, 23, 13],
-            "n": 9,
-            "lookup": {
-                10: [1, 89, 98, 33, 92, 878],
-                21: [98, 54, 12, 0, 67],
-                34: [43, 28, 4],
-                56: [3, 13, 23, 56, 89, 0, 12, 654, 789, 54],
-                78: [46, 32, 54, 66, 87, 13, 16, 89, 33, 6],
-                89: [13, 33, 0, 12],
-                96: [88, 0, 3, 55, 33],
-            },
-        },
-        {
-            "arr": [23, 43, 84, 62, 31],
-            "n": 5,
-            "lookup": {
-                28: [42, 31, 11, 14, 43],
-                34: [84, 12, 32, 31, 23],
-                45: [43, 0, 2, 23, 84],
-                50: [2, 43],
-                52: [23, 84, 62, 31, 43],
-                69: [0, 2, 43, 60, 30],
-            },
-        },
-        {
-            "arr": [
-                4,
-                13,
-                98,
-                54,
-                88,
-                16,
-            ],
-            "n": 7,
-            "lookup": {
-                113: [65, 89, 98, 33, 65, 878],
-                124: [98, 54, 123, 234, 67],
-                133: [43, 76, 9],
-                141: [4, 13, 23, 56, 89, 00, 87, 654, 789, 54],
-                154: [46, 32, 54, 66, 87, 88, 943, 16, 89, 33, 6],
-                152: [46, 32, 54, 66, 87, 88, 943, 16, 89, 33, 12],
-                188: [13, 33],
-                219: [88, 76, 77, 55, 32],
-                293: [43, 76, 9],
-                344: [4, 13, 23, 56, 89, 00, 87, 654, 789, 54],
-                395: [46, 32, 54, 66, 87, 88, 943, 16, 89, 33, 6],
-            },
-        },
-        {
-            "arr": [4, 13, 98, 54, 88, 16, 33],
-            "n": 7,
-            "lookup": {
-                13: [65, 89, 98, 33, 65, 878],
-                24: [98, 54, 123, 234, 67],
-                33: [43, 76, 9],
-                41: [4, 13, 23, 56, 89, 00, 87, 654, 789, 54],
-                54: [46, 32, 54, 66, 87, 88, 943, 16, 89, 33, 6],
-                88: [13, 33],
-                98: [88, 76, 77, 55, 32],
-            },
-        },
-        {
-            "arr": [1, 2, 3, 4, 5, 6],
-            "n": 6,
-            "lookup": {
-                11: [1, 3, 4, 9, 7],
-                22: [7, 1, 2, 3, 4, 5, 6],
-                33: [9, 2, 3, 4, 5, 6],
-                44: [10, 23, 12, 33, 1],
-            },
-        },
-    ]
+def sold():
+    with open("/home/aban/Desktop/Python/problem solving/small_testcase2.txt", "r") as f:
+        maxi = None
+        c = 1
+        lookup = None
+        hash_map = {}
+        res_map = {}
+        for item in f:
+            if c == 2:
+                for o in json.loads(item):
+                    hash_map[o] = o
+            if c == 3:
+                lookup = json.loads(item)
+                for i in lookup:
+                    res_map[i] = 0
+                    for j in lookup[i]:
+                        if j in hash_map:
+                            res_map[i] += 1
+                    if maxi == None:
+                        maxi = i
+                    else:
+                        if res_map[maxi] > res_map[i]:
+                            maxi = maxi
+                        else:
+                            maxi = i
+                print(maxi)
+                print([k for k, v in res_map.items() if v == res_map[maxi]])
+                # print(list(res_map.keys()))
+            c += 1
 
-    for test_case in test_cases:
-        print(solution(test_case["n"], test_case["arr"], test_case["lookup"]))
+# def solo():
+#     with open("/home/aban/Desktop/Python/problem solving/large_testcase2.txt", "r") as f:
+#         maxi = None
+#         c = 1
+#         lookup = None
+#         hash_map = {}
+#         res_map = {}
+#         for item in f:
+#             if c == 3:
+#                 json.loads(item)
+#             c += 1
+sold()
 
 
-test_solution()
+print(datetime.datetime.now() - s)
